4e15833aac4406e7915295cfdf1e0af6927a50b8
Rama (al) <77774837+gfrrmd@users.noreply.github.com>
2026-08-28 17:38:26 +0700

    Add files via upload

--- utils/media.py
+++ utils/media.py
@@ -1,8 +1,8 @@
 import io
 
-from telethon.tl.types import DocumentAttributeVideo, DocumentAttributeAudio, MessageMediaDocument, MessageMediaPhoto
+from telethon.tl.types import DocumentAttributeVideo, MessageMediaDocument, MessageMediaPhoto
 
-from utils.helpers import get_file_name, get_video_attributes, is_sticker_doc, is_voice_note, is_round_video
+from utils.helpers import get_file_name, get_video_attributes, is_sticker_doc
 from utils.progress import make_upload_progress
 
 
@@ -16,27 +16,9 @@ async def _send_media_file(client, msg, media_bytes, status_msg, caption="", tas
     elif isinstance(msg.media, MessageMediaDocument):
         doc = msg.media.document
         mime = getattr(doc, "mime_type", "") or ""
-
-        # Voice note (prioritas sebelum audio umum)
-        if is_voice_note(msg):
-            file_obj.name = get_file_name(doc) or "voice.ogg"
-            await client.send_file("me", file=file_obj, voice_note=True, caption=caption, parse_mode="markdown", progress_callback=up_cb)
-
-        # Stiker
-        elif is_sticker_doc(doc):
+        if is_sticker_doc(doc):
             file_obj.name = "sticker.tgs" if "tgsticker" in mime else ("sticker.webm" if "video" in mime else "sticker.webp")
             await client.send_file("me", file=file_obj, force_document=False, progress_callback=up_cb)
-
-        # Round video (video note bulat)
-        elif is_round_video(msg):
-            video_attr = get_video_attributes(doc)
-            file_obj.name = get_file_name(doc) or "video.mp4"
-            attrs = []
-            if video_attr:
-                attrs = [DocumentAttributeVideo(duration=video_attr.duration, w=video_attr.w, h=video_attr.h, supports_streaming=False, round_message=True)]
-            await client.send_file("me", file=file_obj, caption=caption, parse_mode="markdown", attributes=attrs or None, allow_cache=False, progress_callback=up_cb)
-
-        # Video biasa
         elif "video" in mime or "mp4" in mime:
             video_attr = get_video_attributes(doc)
             file_obj.name = get_file_name(doc) or "video.mp4"
@@ -44,22 +26,9 @@ async def _send_media_file(client, msg, media_bytes, status_msg, caption="", tas
             if video_attr:
                 attrs = [DocumentAttributeVideo(duration=video_attr.duration, w=video_attr.w, h=video_attr.h, supports_streaming=True, round_message=False)]
             await client.send_file("me", file=file_obj, caption=caption, parse_mode="markdown", attributes=attrs or None, allow_cache=False, progress_callback=up_cb)
-
-        # Foto sebagai document
         elif mime in ("image/jpeg", "image/png", "image/webp"):
             file_obj.name = "photo" + {"image/jpeg": ".jpg", "image/png": ".png", "image/webp": ".webp"}.get(mime, ".jpg")
             await client.send_file("me", file=file_obj, caption=caption, parse_mode="markdown", force_document=False, allow_cache=False, progress_callback=up_cb)
-
-        # Audio biasa
-        elif "audio" in mime:
-            fname = get_file_name(doc) or "audio"
-            ext = {"audio/mpeg": ".mp3", "audio/ogg": ".ogg"}.get(mime, "")
-            if "." not in fname:
-                fname += ext
-            file_obj.name = fname
-            await client.send_file("me", file=file_obj, caption=caption, parse_mode="markdown", force_document=False, allow_cache=False, progress_callback=up_cb)
-
-        # Dokumen lainnya (PDF, GIF, dll)
         else:
             fname = get_file_name(doc) or "document"
             if "." not in fname:
